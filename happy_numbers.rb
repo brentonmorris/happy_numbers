b6082c997f2c49bc129e1744821a6af6efbad00b
Brenton Morris <bmorris@2u.com>
2019-03-14 18:10:10 -0400

    Add iterative solution

--- happy_numbers.rb
+++ happy_numbers.rb
@@ -1,17 +1,34 @@
-def calculate_happiness(number, curr_count)
-  result = number.to_s.split("")
-
-  curr_val = result.map { |digit| digit.to_i ** 2 }.reduce(:+)
+def re_calculate_happiness(number, curr_count)
+  curr_val = sum_of_square_digits(number)
 
   if curr_count == 8
-    puts "Sad Number! #{curr_val}"
-    return
+    return false
   end
 
   if curr_val == 1
-    puts "Happy Number! #{curr_val}"
-    return
+    return true
   end
 
-  calculate_happiness(curr_val, curr_count + 1)
+  re_calculate_happiness(curr_val, curr_count + 1)
+end
+
+
+def iter_calculate_happiness(number)
+  seen_numbers = []
+
+  number = sum_of_square_digits(number)
+  while number != 1 do
+    number = sum_of_square_digits(number)
+
+    if seen_numbers[number]
+      return false
+    end
+    seen_numbers[number] = true
+  end
+
+  return true
+end
+
+def sum_of_square_digits(number)
+  number.to_s.split('').map { |digit| digit.to_i ** 2 }.reduce(:+)
 end
